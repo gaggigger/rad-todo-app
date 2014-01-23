dbe972e8d33707409895a33c29176a313122c2c6
Micah Roberson <micah.roberson@gmail.com>
2014-01-23 13:25:12 -0800

    Remove links from header for modifying todos. Also remove all todos sub views

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -2,14 +2,6 @@
 
 angular.module('toptal_todo.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
-
-    $scope.menu = [{
-        'title': 'Todos',
-        'link': 'todos'
-    }, {
-        'title': 'Create New Todo',
-        'link': 'todos/create'
-    }];
     
     $scope.isCollapsed = false;
 }]);
